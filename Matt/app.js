2615890597549a1335b695aba0fb28fe8cff63f1
Matthew <mcraig150@gmail.com>
2020-08-07 00:27:05 -0400

    Cleaned up the code and added comments for app.js

--- Matt/app.js
+++ Matt/app.js
@@ -1,90 +1,88 @@
-const key = 'cxyhnll5ScFfHdAHD2pKJ0hAOREZIlaSFRx6MnajQy35qxKJVn';
+//user secret to be moved to .env file
+const key = "cxyhnll5ScFfHdAHD2pKJ0hAOREZIlaSFRx6MnajQy35qxKJVn";
+const secret = "O4kyEkoJkbFd5OfhhPaYja1c8GHtSPyI9W1eCyBc";
 
-const secret = 'O4kyEkoJkbFd5OfhhPaYja1c8GHtSPyI9W1eCyBc';
-
+//variable to store image form api call
 var image;
 
-var x = 0;
-
-
+//function that calls for access token 
 async function getToken() {
-    let parm = new URLSearchParams();
+  let parm = new URLSearchParams();
 
-    parm.append("grant_type", "client_credentials")
-    parm.append("client_id", key);
-    parm.append("client_secret",secret)
+  //creating url parameters
+  parm.append("grant_type", "client_credentials");
+  parm.append("client_id", key);
+  parm.append("client_secret", secret);
 
-    const petToken = await fetch(
-        'https://api.petfinder.com/v2/oauth2/token',
-        {
-            method: 'POST',
-            body: parm
-        }
-    )
+  const petToken = await fetch("https://api.petfinder.com/v2/oauth2/token", {
+    method: "POST",
+    body: parm,
+  });
 
-    let obj = await petToken.json();
+  let obj = await petToken.json();
 
-
-    console.log(obj);
-     sendData(obj)
+  console.log(obj);
+  sendData(obj);
 }
 
+//function that calls api after search button is clicked
 async function sendData(data) {
-    console.log(data.access_token);
+  console.log(data.access_token);
 
-    $("#searchBtn").click(async () => {
-        event.preventDefault();
+  $("#searchBtn").click(async () => {
+    event.preventDefault();
 
-        $("#cards").empty();
+    //removes existing cards for new search
+    $("#cards").empty();
 
-        let zipCode = $("#zip").val();
-        let type = $("#animalType").val();
+    //sets zip code and type to the values of the form
+    let zipCode = $("#zip").val();
+    let type = $("#animalType").val();
 
-        console.log(zipCode + " " + type)
+    console.log(zipCode + " " + type);
 
-
+    //api call adding user input
     const resp = await fetch(
-        `https://api.petfinder.com/v2/animals?type=${type}&location=${zipCode}`,
-        {
-            headers: {
-                Authorization: `Bearer ${data.access_token}`,
-                Accept: '*/*'
-            }
-        });
-    
+      `https://api.petfinder.com/v2/animals?type=${type}&location=${zipCode}`,
+      {
+        headers: {
+          Authorization: `Bearer ${data.access_token}`,
+          Accept: "*/*",
+        },
+      }
+    );
 
     const respData = await resp.json();
     renderCard(respData);
 
     if (respData == null) {
-
-      console.log('retrieving data')
-      return sendData(respData)
-
-    } 
-    else {
-
-      console.log('success')
+      console.log("retrieving data");
+      return sendData(respData);
+    } else {
+      console.log("success");
       //console.log(respData)
       return respData;
-
     }
-;    });
-
+  });
 }
 
+//function that generates cards for each animal
 function renderCard(info) {
-    console.log(info);
+  console.log(info);
+  //variable to count how many cards are created
+  let x = 0;  
 
-    for(let i in info.animals){
-        console.log(info.animals[i].name);
+  //loop that appends the html to addeach card
+  for (let i in info.animals) {
+    console.log(info.animals[i].name);
 
-        if(info.animals[i].photos.length != 0) {
-            image = info.animals[i].photos[0].full;
-        }
-        
-        $("#cards").append(
-            `
+    //checks to see if there is an image to prevent erroring out 
+    if (info.animals[i].photos.length != 0) {
+     image = info.animals[i].photos[0].full;
+    }
+
+    $("#cards").append(
+      `
             <div>
                 <div class="col s6 m6">
                     <div class="card">
@@ -102,32 +100,31 @@ function renderCard(info) {
                 </div>
             </div>
         `
-        );
-       x++;
-    }
-    
-    saveData(x);
+    );
+    x++;
+  }
+
+  saveData(x);
 }
 
+//function that is used to save data when button is clicked takes in count as a parameter which tells the function how many buttons were created
 function saveData(count) {
+  //loops through each button and adds an event listener to it   
+  for (let i = 0; i < count; i++) {
+    $("#fav" + i).click(() => {
+      $("#cards").append(" <h1> test </h1>");
+      //creates an object array with the cards info and logs it to the console on click  
+      let fav = [
+        {
+          distance: $("#dist" + i).text(),
+          image: $("#img" + i).attr("src"),
+          url: $("#url" + i).attr("href"),
+        },
+      ];
 
-    for(let i = 0; i < count; i++) {
-        $("#fav"+i).click(() => {
-
-            $("#cards").append(" <h1> test </h1>");
-        
-            let fav = [{
-                distance: $("#dist" + i).text(),
-                image: $("#img" + i).attr("src"),
-                url: $("#url" + i).attr("href")
-            }]
-
-            console.log(fav);
-        });
-    }
-
+      console.log(fav);
+    });
+  }
 }
-
-
 
 getToken();
