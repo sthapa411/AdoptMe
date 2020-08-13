9d52033969829f52225b0214d391f285498a9283
Rebecca Capirchio <45076316+rlizm100@users.noreply.github.com>
2020-08-13 15:03:03 -0400

    add adoptme

--- models/adoptme.js
+++ models/adoptme.js
@@ -1,1 +1,18 @@
+var orm = require("../config/orm.js");
 
+var adoptme = {
+  selectAll: function(cb) {
+    orm.all("animal", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("animal", cols, vals, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (adoptmeController.js).
+module.exports = adoptme;
