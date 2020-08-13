df0ad4879c659d6b4960f31e5c719db0b6b59800
Rebecca Capirchio <45076316+rlizm100@users.noreply.github.com>
2020-08-13 15:02:14 -0400

    add schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,14 @@
+-- Drops the adoptme if it exists currently --
+DROP DATABASE IF EXISTS adoptme_db;
+-- Creates the "adoptme_db" database --
+CREATE DATABASE adoptme_db;
 
+USE adoptme_db;
+
+CREATE TABLE animal (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    image VARCHAR(255) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
