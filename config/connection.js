e345a6f6fcd3f5b6667a664d5f70c12a8dfd2103
sthapa <san20hal@gmail.com>
2020-08-15 14:03:34 -0400

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,18 @@
-// Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+     // DB is local on localhost
+connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
-  password: "@My20halgit",
-  database: "adoptme_db"
+  password: "hacktheplanet",
+  database: "todoagain_db"
 });
+}
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
-// Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
